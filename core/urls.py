c9466b4c6582a66677698000b1c74adb2163c991
samwel2000 <samwelgfrey@gmail.com>
2021-08-01 12:48:06 -0500

    -

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('fpc_api.urls')),
-]
+] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
